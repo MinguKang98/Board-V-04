9d24b49f9027785cbce00aed6e8df5d5093d2c7a
mingu <alsrn7590@naver.com>
2022-07-25 17:11:02 +0900

    init

---
+++ boardv04-back/src/main/java/com/study/boardv04back/Boardv04BackApplication.java
@@ -0,0 +1,13 @@
+package com.study.boardv04back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boardv04BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boardv04BackApplication.class, args);
+    }
+
+}
