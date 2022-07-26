951aff85adbf7c111a6066a9ab5b8f42a998e394
mingu <alsrn7590@naver.com>
2022-07-26 18:45:17 +0900

    feat: Board.java 메서드 updateBoard 추가
    Board.java 수정 : updateBoard 추가

--- boardv04-back/src/main/java/com/study/boardv04back/domain/Board.java
+++ boardv04-back/src/main/java/com/study/boardv04back/domain/Board.java
@@ -1,14 +1,13 @@
 package com.study.boardv04back.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.apache.ibatis.type.Alias;
 
 /**
  * 게시글을 나타내는 클래스
  */
 @Getter
+@Setter(value = AccessLevel.PRIVATE)
 @Alias(value = "Board")
 @NoArgsConstructor
 public class Board {
@@ -41,6 +40,44 @@ public class Board {
         this.content = content;
         this.fileExist = fileExist;
         this.categoryId = categoryId;
+    }
+
+    /**
+     *
+     * @param inputUser
+     * @param inputTitle
+     * @param inputContent
+     * @param inputFileExist
+     * @return
+     */
+    public Board updateBoard(String inputUser,
+                             String inputTitle,
+                             String inputContent,
+                             Boolean inputFileExist) {
+
+        int boardId = this.getBoardId();
+        String originUser = this.getUser();
+        String originTitle = this.getTitle();
+        String originContent = this.getContent();
+        Boolean originFileExist = this.getFileExist();
+
+
+        Board newBoard = new Board();
+        newBoard.setBoardId(boardId);
+        if (!originUser.equals(inputUser)) {
+            newBoard.setUser(inputUser);
+        }
+        if (!originTitle.equals(inputTitle)) {
+            newBoard.setTitle(inputTitle);
+        }
+        if (!originContent.equals(inputContent)) {
+            newBoard.setContent(inputContent);
+        }
+        if (originFileExist != inputFileExist) {
+            newBoard.setFileExist(inputFileExist);
+        }
+
+        return newBoard;
     }
 
 }
