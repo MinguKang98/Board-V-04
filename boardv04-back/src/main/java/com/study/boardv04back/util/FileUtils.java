d4f7e7930313eb5cb8e684a59aca31bebb8e95f2
mingu <alsrn7590@naver.com>
2022-07-27 16:43:08 +0900

    feat: FileUtils.java getFileResource ㅔ메서드 추가, 메서드 주석 추가
    getFileResource 추가
    주석 추가

--- boardv04-back/src/main/java/com/study/boardv04back/util/FileUtils.java
+++ boardv04-back/src/main/java/com/study/boardv04back/util/FileUtils.java
@@ -3,15 +3,22 @@ package com.study.boardv04back.util;
 import com.study.boardv04back.domain.File;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * File 관련 메서드 제공 클래스
+ */
 @Component
 public class FileUtils {
 
@@ -19,11 +26,19 @@ public class FileUtils {
     private String DOWNLOAD_DIR;
     private String SEPARATOR = java.io.File.separator;
 
-    public List<File> getAddFileList(List<MultipartFile> multipartFileList ) throws IOException {
+    /**
+     * MultipartFile의 List를 입력받아 해당 파일을 저장하고 해당 MultiPartFile들을
+     * File의 List로 변경 후 return
+     *
+     * @param multipartFileList : 다운로드하고 변환할 MultiPartFile의 List
+     * @return : 추가할 File의 list
+     * @throws IOException
+     */
+    public List<File> getAddFileList(List<MultipartFile> multipartFileList) throws IOException {
 
         List<File> fileList = new ArrayList<>();
         for (MultipartFile multipartFile : multipartFileList) {
-            if (multipartFile !=null && !multipartFile.isEmpty()) {
+            if (multipartFile != null && !multipartFile.isEmpty()) {
                 String originFileName = multipartFile.getOriginalFilename();
                 String fileExtension = FilenameUtils.getExtension(originFileName).toLowerCase();
                 String systemFileName = UUID.randomUUID() + "." + fileExtension;
@@ -43,11 +58,20 @@ public class FileUtils {
         return fileList;
     }
 
+    /**
+     * MultipartFile의 List를 입력받아 해당 파일을 저장하고 해당 MultiPartFile들을
+     * File의 List로 변경 후 return
+     *
+     * @param multipartFileList : 다운로드하고 변환할 MultiPartFile의 List
+     * @param boardId           : File로 변환 시 추가할 File의 boardId
+     * @return : 추가할 File의 list
+     * @throws IOException
+     */
     public List<File> getAddFileList(List<MultipartFile> multipartFileList, int boardId) throws IOException {
 
         List<File> fileList = new ArrayList<>();
         for (MultipartFile multipartFile : multipartFileList) {
-            if (multipartFile !=null && !multipartFile.isEmpty()) {
+            if (multipartFile != null && !multipartFile.isEmpty()) {
                 String originFileName = multipartFile.getOriginalFilename();
                 String fileExtension = FilenameUtils.getExtension(originFileName).toLowerCase();
                 String systemFileName = UUID.randomUUID() + "." + fileExtension;
@@ -81,5 +105,20 @@ public class FileUtils {
 //            }
         }
         return fileList;
+    }
+
+    /**
+     * File을 Resource로 변경
+     *
+     * @param file : 변경할 File
+     * @return : 입력받은 File의 Resource
+     * @throws IOException
+     */
+    public Resource getFileResource(File file) throws IOException {
+
+        java.io.File downloadFile = new java.io.File(DOWNLOAD_DIR + SEPARATOR + file.getSystemName());
+        InputStreamResource resource = new InputStreamResource((new FileInputStream(downloadFile)));
+
+        return resource;
     }
 }
