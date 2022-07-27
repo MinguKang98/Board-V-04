d866ac507eb4c326cb7ecd26c0f81ddea3611f94
mingu <alsrn7590@naver.com>
2022-07-28 00:44:29 +0900

    refactor: CommentController.java saveComment 메서드 HttpStatus 수정
    saveComment 수정 : HttpStatus OK에서 CREATED로 수정

--- boardv04-back/src/main/java/com/study/boardv04back/controller/CommentController.java
+++ boardv04-back/src/main/java/com/study/boardv04back/controller/CommentController.java
@@ -35,7 +35,7 @@ public class CommentController {
 
         commentService.addComment(comment);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 }
