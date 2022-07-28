d2efdcfeb1e6cbf5595f9b7e1bd6b56527fa0e58
mingu <alsrn7590@naver.com>
2022-07-29 01:51:28 +0900

    feat: index.js /list 경로 추가, index.html bootstrap, fontawesome 의존성 추가, App.vue navigation bar 수정

--- boardv04-front/src/App.vue
+++ boardv04-front/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="navbar">
+      <div class="container">
+        <router-link to="/list" class="navbar-brand">Home</router-link>
+      </div>
     </nav>
     <router-view/>
   </div>
